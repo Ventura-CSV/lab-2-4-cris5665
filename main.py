9df3267dd8a8cecfe94c2e13ea4788412864126c
“Cristian <ctoledo5665@gmail.comgit config --global user.name cris5665git config --global user.email ctoledo5665@gmail.com>
2023-08-25 15:59:44 -0700

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    original_str = "Python Programming"
+    sub1= original_str.replace("Python Programming", "Python ")
+    sub2= original_str.replace("Python Programming", "Programming")
+    merge_str= sub1 + sub2
+    print(sub1)
+    print(sub2)
+    print(merge_str)
     pass
 
 
